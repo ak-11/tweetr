7f49cffeca576969d87ed011ad8fa46d46e31684
Andrew Kim <your@email.com>
2017-01-25 17:12:04 -0500

    Removed commented code.

--- public/scripts/toggle-form.js
+++ public/scripts/toggle-form.js
@@ -1,15 +1,6 @@
 $(() => {
-  // $('#compose-button').on('click', () => {
-  //   $('#compose').toggle('display')
-  //     $('#new-tweet').focus()
-  // })
-  // $('#compose-button').on('click', () => {
-  //   $('#compose').toggle()
-  //     $('#new-tweet').focus()
-  // })
   $('#compose-button').on('click', () => {
     $('#compose').slideToggle('fast')
     $('#new-tweet').focus()
   })
-
 })
